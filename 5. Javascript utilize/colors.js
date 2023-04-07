31255ab0b2b93eecee10e6cba26a9fbea0bffa7e
thelight0804 <thelight0804@gmail.com>
2023-04-07 23:00:56 +0900

    Study separate JS file
    파일로 쪼개서 정리 정돈하기

---
+++ 5. Javascript utilize/colors.js
@@ -0,0 +1,34 @@
+var Links = {
+  SetColor : function(color){
+  var aList = document.querySelectorAll('a');
+  var i = 0;
+    while(i < aList.length){
+      aList[i].style.color = color;
+      i++
+    }
+  }
+}
+
+var Body = {
+  SetColor : function(target, color) {
+    target.style.color = color;
+  },
+  SetBackgroundColor : function(target, color){
+  target.style.backgroundColor = color;
+  }
+}
+
+function nightDayHandler(self) {
+  var target = document.querySelector('body');
+  if (self.value === 'night') {
+    Body.SetBackgroundColor(target, 'black')
+    Body.SetColor(target, 'white')
+    self.value = 'day';
+    Links.SetColor('powderblue');
+  } else {
+    Body.SetBackgroundColor(target, 'white')
+    Body.SetColor(target, 'black')
+    self.value = 'night';
+    Links.SetColor('blue');
+  }
+}
