69d74b901ce384809b98af5808f677814af2082b
thelight0804 <thelight0804@gmail.com>
2023-04-10 17:40:42 +0900

    Study Object basic CRUD

--- 6. Javascript object orientation/ex6_1.js
+++ 6. Javascript object orientation/ex6_1.js
@@ -1,1 +1,16 @@
-console.log('Hello OOP');
+var memberArr = ['Tanjiro', 'Nezuko', 'Zenitsu', 'Inosuke'];
+console.log("memberArr[1]: ", memberArr[1]);
+
+var memberObj = {
+  black: 'Tanjiro',
+  pink: 'nezuko',
+  yellow: 'Zenitsu',
+  skyBlue: 'Inosuke'
+}
+
+memberObj.pink = 'Nezuko';
+console.log("memberObj.pink: ", memberObj.pink);
+console.log("memberObj['pink']: ", memberObj['pink']);
+
+delete memberObj.pink;
+console.log("delete memberObj.pink: ", memberObj.pink);
