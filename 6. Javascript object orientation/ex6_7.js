cfe73b6909430d6d6cf8f7569babe3cd03d4c486
thelight0804 <thelight0804@gmail.com>
2023-04-14 12:20:29 +0900

    Study use prototype
    prototype을 사용하여 객체 안에 있던 메서드를 바깥에서 정의하였습니다

---
+++ 6. Javascript object orientation/ex6_7.js
@@ -0,0 +1,20 @@
+function Gundam(name, first, second, third){
+  this.name = name;
+  this.first = first;
+  this.second = second;
+  this.third = third;
+}
+
+Gundam.prototype.sum = function(){
+  return this.first + this.second + this.third;
+}
+
+var Miorine = new Gundam('Miorine Rembran', 10, 20, 30);
+Miorine.sum = function(){
+  return this.first + this.second;
+}
+
+var Suletta = new Gundam('Suletta Mercury', 5, 50, 10);
+
+console.log(Miorine.sum())
+console.log(Suletta.sum())
