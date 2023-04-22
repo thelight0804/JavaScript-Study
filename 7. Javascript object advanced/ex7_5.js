01741d7efe90c70a22956255cbdbcadb986368de
thelight0804 <thelight0804@gmail.com>
2023-04-22 15:54:32 +0900

    Study connect super object to Object.create()
    Object.create를 사용하여 자식에서 부모 객체의 prototype 메서드를 연결했습니다

--- 7. Javascript object advanced/ex7_5.js
+++ 7. Javascript object advanced/ex7_5.js
@@ -9,13 +9,18 @@ Gundam.prototype.sum = function(){
 }
 
 function GundamPlus(name, first, second, third, fourth){
-  Gundam(name, first, second, third);
+  Gundam.call(this, name, first, second, third);
   this.fourth = fourth;
 }
+// GundamPlus.prototype.__proto__ = Gundam.prototype;
+GundamPlus.prototype = Object.create(Gundam.prototype);
+
 GundamPlus.prototype.age = function(){
   return (this.first + this.second + this.third + this.fourth) / 4;
 }
 
 var Suletta = new GundamPlus('Suletta Mercury', 5, 50, 10, 20);
 
-console.log(Suletta.sum());
+console.log(Suletta.age()); //21.25
+console.log(Suletta.sum()); //65
+console.log(Suletta.constructor);
